642d6e338e8e9cfc1c45e03c2ce8f26b22eee3ec
Rohidul <rohidul209@gmail.com>
2022-05-12 23:47:18 +0600

    Tailwind css added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>;
+  return <div className="text-3xl font-bold">Hello world</div>;
 };
 
 export default Home;
